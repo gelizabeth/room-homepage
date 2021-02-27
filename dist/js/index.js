7f870f9e4618a7be51122f47f0b8490eaed8b1e5
Liza <liza.guyvan1@gmail.com>
2021-02-27 01:05:15 +0100

    mobile-styles: menu add, home slider add; script: burger button complete, slider buttons add, have to add next/prev with keyboard arrows

---
+++ dist/js/index.js
@@ -0,0 +1,68 @@
+console.log('script loaded');
+
+//navigation burger actions
+const menuBtn = document.querySelector('.menu-btn__burger');
+const burger = document.querySelector('.menu-btn__burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const logo = document.querySelector('.nav__logo');
+
+let isMenuOpen = false;
+
+const openMenu = () => {
+ if(isMenuOpen){
+    burger.classList.remove('open');
+    logo.classList.remove('close');
+    nav.classList.remove('open');
+    menuNav.classList.remove('open');
+ } else {
+     burger.classList.add('open');
+     logo.classList.add('close');
+     nav.classList.add('open');
+     menuNav.classList.add('open');
+ }
+ isMenuOpen = !isMenuOpen;
+}
+menuBtn.addEventListener('click', openMenu);
+
+//slider
+const prevBtn = document.querySelector('.slider__button_prev');
+const nextBtn = document.querySelector('.slider__button_next');
+const slides = document.querySelectorAll('.slide');
+const homeContents = document.querySelectorAll('.home__content');
+
+let currentSlide = 0;
+
+const activeSlide = (currentSlide) => {
+    slides.forEach((slide, index) => {
+        if(index === currentSlide){
+            slide.classList.add('slide_active');
+        } else slide.classList.remove('slide_active');
+    });
+    homeContents.forEach((slide, index) => {
+        if(index === currentSlide){
+            slide.classList.add('home__content_active');
+        } else slide.classList.remove('home__content_active');
+    });
+}
+
+const next = () => {
+    if(currentSlide === slides.length-1) {
+        currentSlide = 0;
+    } else currentSlide+=1;
+    console.log(currentSlide);
+    activeSlide(currentSlide);
+}
+const prev = () => {
+    if(currentSlide === 0) {
+        currentSlide = slides.length-1;
+    } else currentSlide-=1;
+    console.log(currentSlide);
+    activeSlide(currentSlide);
+}
+
+
+activeSlide(currentSlide);
+
+prevBtn.addEventListener('click', prev);
+nextBtn.addEventListener('click', next);
